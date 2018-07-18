2ced538d2898fda9c19d6a17da1671559eebaea7
gstoian <george.stoian@endava.com>
2018-07-18 22:41:54 +0300

    EntityFramework Database-First setup. Added a first example of Entity, Repository and logic component.

---
+++ TheAMTeam/TheAMTeam.App/Example/Example.cs
@@ -0,0 +1,32 @@
+﻿using TheAMTeam.Data;
+using TheAMTeam.Data.Repositories;
+
+namespace TheAMTeam.App.Example
+{
+    public static class Example
+    {
+        private static readonly TestEntityRepository _testEntityRepository = new TestEntityRepository();
+        public static void Execute()
+        {
+            var testEntity = CreateTestEntity();
+            var savedTestEntity = SaveTestEntity(testEntity);
+        }
+
+        private static TestEntity CreateTestEntity()
+        {
+            var testEntity = new TestEntity
+            {
+                Message = "I am a new Test Entry."
+            };
+
+            return testEntity;
+        }
+
+        private static TestEntity SaveTestEntity(TestEntity testEntity)
+        {
+            var savedTestEntity = _testEntityRepository.Create(testEntity);
+
+            return savedTestEntity;
+        }
+    }
+}
--- TheAMTeam/TheAMTeam.App/Program.cs
+++ TheAMTeam/TheAMTeam.App/Program.cs
@@ -1,15 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TheAMTeam.App
+﻿namespace TheAMTeam.App
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            Example.Example.Execute();
+
+            //Add your code here
         }
     }
 }
