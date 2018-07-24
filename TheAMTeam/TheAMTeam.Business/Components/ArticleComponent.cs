67d64ce3e1903a4ab12ca22ef0b0bd40393c8639
tdumitriu <tdumitriu@EN610212>
2018-07-24 14:50:17 +0300

    AddedStructureArticleComponentAndController

---
+++ TheAMTeam/TheAMTeam.Business/Components/ArticleComponent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using TheAMTeam.Business.Models;
+using TheAMTeam.DataAccessLayer.Repositories;
+
+namespace TheAMTeam.Business.Components
+{
+    public class ArticleComponent
+    {
+        private readonly ArticlesRepository _articlesRepository;
+
+        public ArticleComponent()
+        {
+            _articlesRepository = new ArticlesRepository();
+        }
+
+    }
+}
