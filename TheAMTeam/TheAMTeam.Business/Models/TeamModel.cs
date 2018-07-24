1d20f290bbe50fb747e54f670101eb239e62bbbd
Alexandru Lazarean <alexandru.lazarean@endava.com>
2018-07-24 14:52:04 +0300

    "Initial COMMIT"

---
+++ TheAMTeam/TheAMTeam.Business/Models/TeamModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using TheAMTeam.DataAccessLayer.Entities;
+
+namespace TheAMTeam.Business.Models
+{
+    public class TeamModel
+    {
+        public int TeamId { get; set; }
+
+        [StringLength(15)]
+        public string Name { get; set; }
+        [StringLength(15)]
+        public string City { get; set; }
+        [StringLength(15)]
+        public string Coach { get; set; }
+
+        public ICollection<Player> Players { get; set; }
+        public virtual ICollection<Match> Matches { get; set; }
+    }
+}
