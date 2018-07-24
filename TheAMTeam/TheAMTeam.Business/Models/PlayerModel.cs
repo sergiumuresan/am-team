11bd3e7207d418e2e101b8daeddb78f30425a4d5
Paul Gal <paul.gal@endava.com>
2018-07-24 14:51:59 +0300

    InitialCommit

---
+++ TheAMTeam/TheAMTeam.Business/Models/PlayerModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace TheAMTeam.Business.Models
+{
+    public class PlayerModel
+    {
+        public int PlayerId { get; set; }
+
+        public string Name { get; set; }
+
+        public int TeamId { get; set; }
+
+        public int TshirtNO { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public string NameAlias { get; set; }
+
+        public int? NationalityId { get; set; }
+        
+        public virtual TeamModel Team { get; set; }
+    }
+}
