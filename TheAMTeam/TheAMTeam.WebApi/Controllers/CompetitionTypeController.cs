e5f5f35e3ea47a71170d2f3372ffb9fb847d66de
isipos <isipos@EN610349>
2018-07-24 14:52:53 +0300

    Added structure

---
+++ TheAMTeam/TheAMTeam.WebApi/Controllers/CompetitionTypeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TheAMTeam.WebApi.Controllers
+{
+    public class CompetitionTypeController : ApiController
+    {
+        // GET: api/Competitions
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Competitions/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Competitions
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Competitions/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Competitions/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
