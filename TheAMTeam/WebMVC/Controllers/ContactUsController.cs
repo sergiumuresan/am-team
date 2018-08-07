68f31395cdb77410c0dedf67f47fed8c08ba6d71
tdumitriu <tdumitriu@EN610212>
2018-08-07 12:38:32 +0300

    FinishedAllValidationsWithIsModelValid

--- TheAMTeam/WebMVC/Controllers/ContactUsController.cs
+++ TheAMTeam/WebMVC/Controllers/ContactUsController.cs
@@ -34,8 +34,7 @@ namespace TheAMTeam.WebMVC.Controllers
 
             var searchedList = result.Where(x => x.Name.ToUpper().Contains(searchString.ToUpper()));
             
-            if(searchedList.Count(x => x.Id > 0) == 0) ViewBag.Message = "There is no name in the database matching the search word";
-
+            if(searchedList.Count(x => x.Id > 0) == 0) ViewBag.NotFoundMessage = "There is no name in the database matching the search word";
 
             return View(searchedList);
         }
@@ -43,12 +42,10 @@ namespace TheAMTeam.WebMVC.Controllers
 
         //Add a new entry 
         public ActionResult Add()
-        {          
-            var viewModel = new ContactModel
-            {
-                Departments = departmentComponent.GetAll()
-            };
-            // todo aici obiectul intra cu datetime.now in view, trebuie sa fac datetime ul nullable probabil
+        {
+            var viewModel = new ContactModel();
+            
+            ViewBag.Departments = departmentComponent.GetAll();
 
             return View(viewModel);
         }
@@ -56,12 +53,17 @@ namespace TheAMTeam.WebMVC.Controllers
         [HttpPost]
         public ActionResult AddNewEntry(ContactModel myContact)
         {
-            if (myContact.Phone == null || myContact.Name == null || myContact.Email == null || myContact.UserMessage == null || myContact.MessageDate == null) return RedirectToAction("ErrorValidation");
-            if (!(Regex.IsMatch(myContact.Phone, @"^\d+$"))) return RedirectToAction("ErrorValidationPhone");
-          
-            contactComponent.Add(myContact);
+            ViewBag.Departments = departmentComponent.GetAll();
 
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                contactComponent.Add(myContact);                
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Add", myContact);
+            }
         }
 
 
@@ -74,7 +76,7 @@ namespace TheAMTeam.WebMVC.Controllers
                 return RedirectToAction("Index");
             }
 
-            matchingUser.Departments = departmentComponent.GetAll();
+            ViewBag.Departments = departmentComponent.GetAll();
 
             return View(matchingUser);
         }
@@ -82,12 +84,17 @@ namespace TheAMTeam.WebMVC.Controllers
         [HttpPost]
         public ActionResult Update(ContactModel contactModelToUpdate)
         {
-            if (contactModelToUpdate.Phone == null || contactModelToUpdate.Name == null || contactModelToUpdate.Email == null || contactModelToUpdate.UserMessage == null) return RedirectToAction("EditErrorValidation");
-            if (!(Regex.IsMatch(contactModelToUpdate.Phone, @"^\d+$"))) return RedirectToAction("EditErrorValidationPhone");
+            ViewBag.Departments = departmentComponent.GetAll();
 
-            contactComponent.Update(contactModelToUpdate);
-
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                contactComponent.Update(contactModelToUpdate);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View("Edit", contactModelToUpdate);
+            }
         }
 
 
@@ -107,28 +114,6 @@ namespace TheAMTeam.WebMVC.Controllers
             contactComponent.Delete(Id);
 
             return RedirectToAction("Index");
-        }
-
-        // Validations
-
-        public ActionResult ErrorValidation()
-        {
-            return View("ErrorValidation");
-        }
-
-        public ActionResult ErrorValidationPhone()
-        {
-            return View("ErrorValidationPhone");
-        }
-
-        public ActionResult EditErrorValidation()
-        {
-            return View("EditErrorValidation");
-        }
-
-        public ActionResult EditErrorValidationPhone()
-        {
-            return View("EditErrorValidationPhone");
         }
     }
 }
