a4988d1b48efe1755201b32bb18c9eebcf992cbe
Josh <iosua.sipos@endava.com>
2018-10-15 16:37:46 +0300

    "Add Web and WebAPI projecs"

---
+++ TheAMTeam/TheAMTeam.WebApi/Controllers/ContactController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using TheAMTeam.Business.Components;
+using TheAMTeam.Business.Models;
+
+
+namespace TheAMTeam.WebApi.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactComponent _contactComponent = new ContactComponent();
+
+        [HttpGet]
+        [Route("api/contacts")]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var getResult = _contactComponent.GetAllContacts();
+                return Request.CreateResponse(HttpStatusCode.OK, getResult);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpGet]
+        [Route("api/contact/{id}")]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                var result = _contactComponent.GetById(id);
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPost]
+        [Route("api/contact")]
+        public HttpResponseMessage Post([FromBody] ContactModel contact)
+        {
+            try
+            {
+                var result = _contactComponent.Add(contact);
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }  
+            catch(Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+
+        [HttpPut]
+        [Route("api/contact")]
+        public HttpResponseMessage Put([FromBody] ContactModel contact)
+        {
+            try
+            {
+                var result = _contactComponent.Update(contact);
+                return Request.CreateResponse(HttpStatusCode.OK,result);
+            }catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+                
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/contact/{contatId}")]
+        public HttpResponseMessage Delete(int contatId)
+        {
+            try
+            {
+                var result = _contactComponent.Delete(contatId);
+                return Request.CreateResponse(HttpStatusCode.OK, result);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, ex);
+            }
+        }
+    }
+}
---
+++ TheAMTeam/TheAMTeam.WebApi/Controllers/TestController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using TheAMTeam.Business.Components;
+using TheAMTeam.Business.Models;
+
+namespace TheAMTeam.WebApi.Controllers
+{
+    public class TestController : ApiController
+    {
+        private TestComponent _testComponent = new TestComponent();
+
+        public List<TestEntityModel> Get()
+        {
+            var getResult = _testComponent.GetAllTests();
+
+            return getResult;
+        }
+
+
+    }
+}
